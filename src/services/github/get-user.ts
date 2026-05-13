14a496ff311066f9ab0ee1f6b79c98362c0602ec
QLHazyCoder <2825305047@qq.com>
2026-05-14 04:48:44 +0800

    revert: remove github rest api version fallback

--- src/services/github/get-user.ts
+++ src/services/github/get-user.ts
@@ -1,49 +1,14 @@
-import {
-  GITHUB_API_BASE_URL,
-  getGitHubRestApiVersions,
-  githubRestHeaders,
-} from "~/lib/api-config"
+import { GITHUB_API_BASE_URL, githubHeaders } from "~/lib/api-config"
 import { HTTPError } from "~/lib/error"
 
-async function isUnsupportedApiVersionResponse(
-  response: Response,
-): Promise<boolean> {
-  if (response.status !== 400) {
-    return false
-  }
-
-  const text = await response
-    .clone()
-    .text()
-    .catch(() => "")
-  return (
-    text.includes("X-GitHub-API-Version")
-    && text.includes("not a supported version")
-  )
-}
-
 export async function getGitHubUser(githubToken: string) {
-  let lastResponse: Response | null = null
+  const response = await fetch(`${GITHUB_API_BASE_URL}/user`, {
+    headers: githubHeaders(githubToken),
+  })
 
-  for (const apiVersion of getGitHubRestApiVersions()) {
-    const response = await fetch(`${GITHUB_API_BASE_URL}/user`, {
-      headers: githubRestHeaders(githubToken, apiVersion),
-    })
+  if (!response.ok) throw new HTTPError("Failed to get GitHub user", response)
 
-    if (response.ok) {
-      return (await response.json()) as GithubUserResponse
-    }
-
-    lastResponse = response
-    if (!(await isUnsupportedApiVersionResponse(response))) {
-      break
-    }
-  }
-
-  throw new HTTPError(
-    "Failed to get GitHub user",
-    lastResponse ?? new Response(null, { status: 500 }),
-  )
+  return (await response.json()) as GithubUserResponse
 }
 
 // Trimmed for the sake of simplicity
